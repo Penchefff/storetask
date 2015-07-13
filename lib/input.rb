0664b76d5034afd10e242c15ea1425c20c402c3b
Penchev <d.d.penchev@gmail.com>
2015-07-13 23:34:10 +0300

    commands input update

--- lib/input.rb
+++ lib/input.rb
@@ -1,9 +1,17 @@
 require_relative 'customer'
 
-class Parser
-  name = gets.chomp
-  ivan = Customer.new(name, 'asd')
-  petkan = Customer.new()
-  puts(ivan.name)
-  puts(petkan.name)
+# class Parser
+
+puts('please enter name and email:')
+name = gets.chomp
+email = gets.chomp
+ivan = Customer.new(name, email)
+puts(ivan.name, ivan.email)
+print('please enter commeand, 1, 2, 3, 4, 5 for exit')
+command = 0
+loop do
+  command = gets.chomp
+  puts(command)
+  break if command == '5'
 end
+# end
