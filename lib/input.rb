76b0af15a22c19eb23849eb9587441969ffe39a0
Penchev <d.d.penchev@gmail.com>
2015-08-20 21:57:27 +0300

    added list all product and some little changes on finctionality

--- lib/input.rb
+++ lib/input.rb
@@ -10,39 +10,33 @@ puts('please enter name and email:')
 name 		= gets.chomp
 email 		= gets.chomp
 customer 	= Customer.new(name: name, email: email)
-items 		= CartItems.new()
-# puts(cart.empty?)
-# product1 = Product.new()
+
 product1 = Product.new(name: 'Meat', price: 12.00)
 product2 = Product.new(name: 'Chiroz', price: 312)
-product3 = Product.new(name: 'asd', price: 312)
+product3 = Product.new(name: 'Bahur', price: 312)
 
 
-# product3 = Product.new('Midi', 123)
-cart1 = Cart.new(asd = CartItems.new())
-iiiteeem = Item.new(product: product1, quantity: 5)
-iiiteeem1 = Item.new(product: product2, quantity: 5)
-iiiteeem2 = Item.new(product: product3, quantity: 5)
-cart1.add(iiiteeem)
-cart1.add(iiiteeem1)
-cart1.add(iiiteeem2)
+cart = Cart.new(asd = CartItems.new())
 
-order = Order.new(customer: customer, items: cart1.items)
-# puts(cart1.total_price())
-# items.add(product1)
-# items.add(product2)
+cart.add(Item.new(product: product1, quantity: 5))
+cart.add(Item.new(product: product2, quantity: 5))
+cart.add(Item.new(product: product3, quantity: 5))
+
+order = Order.new(customer: customer, items: cart.items)
+
 puts('-------------')
 puts(order.items.total_price)
 puts('-------------')
 
 
-puts('please enter command: ')
-puts('1 to add item')
-puts('2 for items check')
-puts('3 for items count')
-puts('4 for items total price')
-puts('6 to add items to cart')
-puts('5 for exit')
+puts('please enter command: )
+1 to list all products
+1 to add item
+2 for items check
+3 for items count
+4 for items total price
+6 to add items to cart
+5 for exit')
 
 asd = Item.new(product: product1, quantity: 3)
 
@@ -53,18 +47,20 @@ loop do
   
   case command
   when '1'
+    puts('list items')
+  when '2'
     name = (gets.chomp)
     price = (gets.chomp)
     items.add(Product.new(name: name, price: price.to_f))
-  when '2'
-    puts(items.empty?)
   when '3'
-    puts(items.count)
+    puts(items.empty?)
   when '4'
-    puts(items.total_price())
+    puts(cart.items.count)
   when '5'
-    break
+    puts(cart.items.total_price())
   when '6'
+    break
+  when '7'
     cart = Cart.new(items)
   end
 end
