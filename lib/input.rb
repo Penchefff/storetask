cc6a7f2c65f8842ba86cd7f81b34d649d45cf361
Penchev <d.d.penchev@gmail.com>
2015-08-20 22:57:54 +0300

    adding product with quantity added

--- lib/input.rb
+++ lib/input.rb
@@ -4,64 +4,56 @@ require_relative 'product'
 require_relative 'item'
 require_relative 'order'
 
-# class Parser
-
 puts('please enter name and email:')
 name 		= gets.chomp
 email 		= gets.chomp
 customer 	= Customer.new(name: name, email: email)
 
-product1 = Product.new(name: 'Meat', price: 12.00)
-product2 = Product.new(name: 'Chiroz', price: 312)
-product3 = Product.new(name: 'Bahur', price: 312)
+meat = Product.new(name: 'Meat', price: 12.00)
+chiroz = Product.new(name: 'Chiroz', price: 312)
+bahur = Product.new(name: 'Bahur', price: 312)
 
+products_list = [meat, chiroz, bahur]
 
-cart = Cart.new(asd = CartItems.new())
+cart = Cart.new(CartItems.new())
 
-cart.add(Item.new(product: product1, quantity: 5))
-cart.add(Item.new(product: product2, quantity: 5))
-cart.add(Item.new(product: product3, quantity: 5))
+cart.add(Item.new(product: meat, quantity: 5))
+cart.add(Item.new(product: chiroz, quantity: 5))
+cart.add(Item.new(product: bahur, quantity: 5))
 
-order = Order.new(customer: customer, items: cart.items)
-
-puts('-------------')
-puts(order.items.total_price)
-puts('-------------')
-
-
-puts('please enter command: )
-1 to list all products
-1 to add item
-2 for items check
-3 for items count
-4 for items total price
-6 to add items to cart
-5 for exit')
-
-asd = Item.new(product: product1, quantity: 3)
-
+def show_menu()
+  puts('please enter command:
+  1 to list all available products
+  2 to add product to cart
+  3 for cart emptyness check
+  4 for cart items count
+  5 for cart items total price
+  6 for exit')
+end
 
 command = 0
 loop do
+  show_menu()
   command = gets.chomp
-  
+
   case command
   when '1'
-    puts('list items')
+    products_list.each_with_index do |product, index|
+      puts format('%s. Name: %s, Price: %s', index, product.name, product.price)
+    end
   when '2'
-    name = (gets.chomp)
-    price = (gets.chomp)
-    items.add(Product.new(name: name, price: price.to_f))
+    puts('Enter product index to add: ')
+    product = products_list[(gets.chomp).to_i]
+    puts('Enter quantity: ')
+    quantity = (gets.chomp).to_f
+    cart.items.add(Item.new(product: product, quantity: quantity))
   when '3'
-    puts(items.empty?)
+    puts(cart.items.empty?)
   when '4'
     puts(cart.items.count)
   when '5'
     puts(cart.items.total_price())
   when '6'
     break
-  when '7'
-    cart = Cart.new(items)
   end
 end
-# end
