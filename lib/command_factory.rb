f8651ac8c680a6f1d5506661c5dee1efb09b0f49
Penchev <d.d.penchev@gmail.com>
2015-08-23 22:12:28 +0300

    command create_customer added to menu

--- lib/command_factory.rb
+++ lib/command_factory.rb
@@ -1,5 +1,7 @@
+require_relative 'create_customer_command'
+
 class CommandFactory
   def self.build
-    []
+    [ CreateCustomerCommand ]
   end
 end
--- lib/menu.rb
+++ lib/menu.rb
@@ -6,6 +6,7 @@ class Menu
   end
   
   def show
+    puts 'Menu:'
     @commands.each do |cmd|
       puts format('%s, code: %s', cmd.msg, cmd.code)
     end
