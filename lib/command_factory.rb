3fb4b90df5bf66bae70f8c92fb70515b69f5ff69
Penchev <d.d.penchev@gmail.com>
2015-09-23 23:14:08 +0300

    cart items total price added to menu and adding product to cart tested

--- lib/command_factory.rb
+++ lib/command_factory.rb
@@ -2,9 +2,10 @@ require_relative 'create_customer_command'
 require_relative 'exit_command'
 require_relative 'list_store_products_command'
 require_relative 'add_product_to_cart_command'
+require_relative 'cart_items_total_price'
 
 class CommandFactory
   def self.build
-    [CreateCustomerCommand, ExitCommand, ListStoreProductsCommand, AddProductToCartCommand]
+    [CreateCustomerCommand, ExitCommand, ListStoreProductsCommand, AddProductToCartCommand, CartItemsTotalPrice]
   end
 end
